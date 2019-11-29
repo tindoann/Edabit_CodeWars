3d5c13ffe32a71b4387c958974ffcfc3fa33301f
tindoann <tindoann@gamil.com>
2019-11-28 20:51:25 -0500

    Day 1b - White

--- koans.js
+++ koans.js
@@ -15,7 +15,26 @@ function add(a, b) {
   if (typeof a !== "number" || typeof b !== "number") {
       return "Sorry but you didn't pass two numbers.";
   }
-  return a + b;
+  return a + b; 
 }
 
 
+// #White lessThanOrEqualToZero(5) ➞ false
+
+// 2. Create a function that takes a number as its only argument and returns true if it's less than or equal 
+//    to zero, otherwise return false.
+
+function lessThanOrEqualToZero(num) {
+	if (num <= 0)
+	  return true;
+	else
+		return false;
+}
+
+// es6
+const lessThanOrEqualToZero = num => num <= 0;
+
+// ternary
+function lessThanOrEqualToZero(num) {
+	return num <= 0 ? true : false;
+}
