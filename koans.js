e6769a1e56a83ddd2065e8d8f53addd3f5245e74
tindoann <tindoann@gamil.com>
2019-11-29 23:58:24 -0500

    Day2e - White

--- koans.js
+++ koans.js
@@ -107,3 +107,30 @@ function addition(num) {
 }
 
 const addition = (num) => num++
+
+// #Whtie Level -  animals(2, 3, 5) ➞ 36
+
+/*
+
+In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+chickens = 2 legs
+cows = 4 legs
+pigs = 4 legs
+The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+
+*/
+
+function animals(chickens, cows, pigs) {
+	chickens *= 2;
+	cows *= 4;
+	pigs *= 4;
+	return chickens + cows + pigs;
+}
+
+function animals(chickens, cows, pigs) {
+	return chickens*2+(cows+pigs)*4;
+}
+
+const animals = (chickens, cows, pigs) => chickens * 2 + cows * 4 + pigs * 4;
+
