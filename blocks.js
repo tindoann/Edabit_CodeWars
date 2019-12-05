c3f32a522794b96d564e475ae6da492b9fdff945
tindoann <tindoann@gamil.com>
2019-12-05 07:38:56 -0500

    Day7a - White

--- blocks.js
+++ blocks.js
@@ -1082,3 +1082,34 @@ function isPlural(word) {
 const isPlural = word => word.endsWith('s');
 
 let isPlural = w => /s$/i.test(w)
+
+
+// 47. Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24. If none of the operations can give 24, return null.
+
+// White - operation(15, 9) ➞ "added"
+
+
+function operation(num1, num2) {
+	if (num1 + num2 == 24) {
+		return 'added'
+	} else if (num1 - num2 == 24) {
+		return 'subtracted'
+	} else if (num1 * num2 == 24) {
+		return 'multiplied'
+	} else if (num1 / num2  == 24) {
+		return 'divided'
+	} else {
+		return null
+	}
+}
+
+const operation=(a,b)=>
+	a+b==24?"added"
+	:a-b==24?"subtracted"
+	:a*b==24?"multiplied"
+	:a/b==24?"divided"
+	:null;
+
+function operation(num1, num2) {
+	return num1 / num2 === 24 ? 'divided' : num1 * num2 === 24 ? 'multiplied' : num1 - num2 === 24 ? 'subtracted' : num1 + num2 === 24 ? 'added' : null
+}
