cf013121c34fd295232b96d2688d790da6068f18
tindoann <tindoann@gamil.com>
2019-12-05 19:38:44 -0500

    Day7c - White

--- blocks.js
+++ blocks.js
@@ -1132,3 +1132,37 @@ const countWords = str => str.split(' ').length;
 const countWords = str => str.replace(/[^\w\s]/gi, '').split(' ').length;
 
 
+/* 49. You are given 2 out of 3 of the angles in a triangle, in degrees.
+
+Write a function that classifies the missing angle as either "acute", "right", or "obtuse" based on its degrees.
+
+An acute angle is less than 90 degrees.
+A right angle is exactly 90 degrees.
+An obtuse angle is greater than 90 degrees (but less than 180 degrees).
+
+For example: missingAngle(11, 20) should return "obtuse", since the missing angle would be 149 degrees, which makes it obtuse.
+
+*/
+
+// White - missingAngle(135, 11) ➞ "acute"
+
+function missingAngle(angle1, angle2) {
+	let x = (180 - (angle1 + angle2))
+	if (x > 90) {
+		return 'obtuse'
+	} else if (x === 90) {
+		return 'right'
+	} else {
+		return 'acute'
+	}
+}
+
+function missingAngle(angle1, angle2) {
+	return angle1 + angle2 < 90 ? 'obtuse'
+		: angle1 + angle2 > 90 ? 'acute'
+		: 'right';
+}
+
+function missingAngle(angle1, angle2) {
+	return angle1 + angle2 === 90 ? 'right' : angle1 + angle2 < 90 ? 'obtuse' : 'acute';
+}
