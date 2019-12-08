1b821315227eb351e5967425fa2c89e1f538757d
tindoann <tindoann@gamil.com>
2019-12-08 08:00:53 -0500

    Day10a - White

--- blocks.js
+++ blocks.js
@@ -1752,3 +1752,44 @@ function replaceVowels(str, ch) {
 	return str.join('');
 }
 
+
+
+/*
+
+68.
+
+A value is said to be "truthy" if it evaluates to true in a Boolean context. All values are truthy in JavaScript unless they're one of the following:
+
+false
+null
+undefined
+0
+NaN
+""
+In JavaScript, an empty object and an empty array are both considered "truthy," but an empty string is considered false when evaluated as a Boolean (this behavior is what we call "falsey").
+
+Create a function that takes an argument of any data type and returns 1 if it's truthy and 0 if it's falsy.
+
+*/
+
+// White - isTruthy("false") ➞ 1
+
+
+function isTruthy(input) {
+	return input ? 1 : 0
+}
+
+isTruthy = (input) => +!!input;
+
+function isTruthy(input) {
+	if(input) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+const isTruthy = (input) => {
+	x = [false,null,undefined,0,NaN,``];
+	return (x.includes(input)?0:1);
+}
